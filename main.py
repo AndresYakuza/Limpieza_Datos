8d1b23b8dfc2dd226b61bc43acdab206bd82c51f
AndresYakuza <andresramos123@outlook.com>
2025-06-24 18:00:41 -0500

    Primera limpieza de datos (Adjunto csv)

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from limpieza-datos!")
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
 
 
-if __name__ == "__main__":
-    main()
+ruta = "dataset_banco.csv"
+data = pd.read_csv(ruta)
+
+print('=====================================================')
+print(data.shape)
+data.head()
+
+print('=====================================================')
+# Vizualizar variables categóricas y las numericas 
+data.info()
+
+print('=====================================================')
+# Limpieza
+
+#Datos faltantes 
+data.dropna(inplace=True)
+data.info()
+
+print('=====================================================')
+#Columnas irrelevantes 
+#(Conteo de los niveles en las diferentes columnas categoricas)
+cols_cat = ['job', 'marital', 'education', 'default', 
+            'housing', 'loan', 'contact', 'month', 'poutcome', 'y']
+
+for col in cols_cat: 
+    print(f'Columna {col}: {data[col].nunique()} subniveles')
+
+data.describe()
+
